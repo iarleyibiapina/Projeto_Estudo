0e420b085a9a47558b8e651429d1cc758b760fe9
iarley <7iarley7@gmail.com>
2024-01-27 17:53:25 -0300

    feat: Adicionado Migration/Model/Factory para criar dados falsos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,16 +13,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        // \App\Models\Marca::factory(10)->create();
+        \App\Models\Produto::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        \App\Models\User::factory()->create([
-            'name' => 'test',
-            'email' => 'um@um.com',
-            'password' => 'admin',
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'test',
+        //     'email' => 'um@um.com',
+        //     'password' => 'admin',
+        // ]);
 
         // para iniciar o seeder
         // php artisan db:seed
