98965c8961c20a2cffb6c3bf8bdb866546095b52
iarley <7iarley7@gmail.com>
2024-02-16 23:46:14 -0300

    #Estudos - DTO - Criando Exemplo DTOs

---
+++ app/DTO/ExemploDto.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DTO;
+
+class ExemploDTO
+{
+    // ESTA dto trabalha com ESTES tipos de DADOS
+
+    // Definindo tipos
+    // public string $name;
+    // public int $idade;
+    // public bool $situacao;
+
+    // feat do php 8, ja define os atributos no construct (sendo public)
+    // readonly outra feat do php 8 , uma vez o dado setado, não pode se alterado
+    public function __construct(
+        public readonly ?string $name,
+        public readonly ?int $idade = 0,
+        public readonly ?bool $situacao = false
+    ) {
+    }
+
+    // definindo padrao
+    // public function __construct(
+    //     string $name,
+    //     int $idade,
+    //     bool $situacao
+    // ) {
+    // $this->name = $name;
+    // $this->idade = $idade;
+    // $this->situacao = $situacao;
+    // }
+}
