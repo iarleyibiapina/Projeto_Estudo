b576aa8ea2801e894d21bce587fcf846a038b5da
iarley <7iarley7@gmail.com>
2024-02-18 14:12:50 -0300

    Feat: Cria Abstract e Interface DTO

---
+++ app/DTO/InterfaceDTO.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Dto;
+
+use Illuminate\Contracts\Validation\Validator;
+
+interface InterfaceDTO
+{
+    public function rules(): array;
+    public function messages(): array;
+    public function validator(): Validator;
+    public function validate(): array;
+}
