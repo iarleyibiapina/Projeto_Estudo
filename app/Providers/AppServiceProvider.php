f0551c8663a263dfff386f233e96ddf8a163f524
iarleyibiapina <7iarley7@gmail.com>
2025-12-15 12:01:41 -0300

    fix: -Update to laravel 12 -Install package debugbar -Adds folder modulation to migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // primeiro abastrata, depois concreta / 
+        // primeiro abastrata, depois concreta /
         // primeiro a esta implementando e depois a que é implementada
         $this->app->bind('App\Repositories\Interfaces\TaskRepositoryInterface', 'App\Repositories\TaskRepository');
         $this->app->bind('App\Repositories\TaskRepository', function () {
@@ -26,6 +26,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerMigrations();
+        $this->registerDebugBar();
+    }
+
+    public function registerMigrations(): void
+    {
+        $migrationsPath = database_path('migrations');
+        $directories = glob($migrationsPath . '/*' , GLOB_ONLYDIR);
+        $paths = array_merge([$migrationsPath], $directories);
+        $this->loadMigrationsFrom($paths);
+    }
+
+    public function registerDebugBar(): void
+    {
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('Debugbar', \Barryvdh\Debugbar\Facades\Debugbar::class);
     }
 }
